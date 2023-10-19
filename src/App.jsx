c010cb6fa8c652a86baa836fa437c73fc5957cf2
hsrvms <59848940+hsrvms@users.noreply.github.com>
2023-10-19 16:14:07 +0300

    added prototype / it will be changed and improved

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,72 @@
-import { useState } from 'react';
-import Navbar from "./components/Navbar/Navbar";
-import Timer from './components/Timer/Timer'
-import Settings from './components/Settings/Settings'
-import "./App.css";
+import { useEffect, useContext } from 'react'
+import Button from './components/Button'
+import CountdownAnimation from './components/CountdownAnimation'
+import SetPomodoro from './components/SetPomodoro'
+import { SettingsContext } from './context/SettingsContext'
 
-function App() {
+const App = () => {
 
-  const [activeBtn, setActiveBtn] = useState("pomodoro");
-	const navList = ["pomodoro", "short-break", "long-break"];
+  const {
+    pomodoro,
+    executing,
+    startAnimate,
+    children,
+    startTimer,
+    pauseTimer,
+    updateExecute,
+    setCurrentTimer,
+    SettingsBtn } = useContext(SettingsContext)
 
-  function handleNavClick(event) {
-    const button = event.target;
-    setActiveBtn(button.name);
-	}
+    useEffect(() => {updateExecute(executing)}, [executing, startAnimate])
 
-	return (
-		<main className="app-container">
-			<h1 className="heading">pomodoro</h1>
-      <Navbar navList={navList} navClick={handleNavClick} activeBtn={activeBtn}/>
-      <Timer />
-      <Settings />
-		</main>
-	);
+  return (
+    <div className="container">
+      <h1>Pomodoro</h1>
+      <small>Be productive the right way.</small>
+      {pomodoro !== 0 ?
+      <>
+        <ul className="labels">
+          <li>
+            <Button 
+              title="Work" 
+              activeClass={executing.active === 'work' ? 'active-label' : undefined} 
+              _callback={() => setCurrentTimer('work')} 
+            />
+          </li>
+          <li>
+            <Button 
+              title="Short Break" 
+              activeClass={executing.active === 'short' ? 'active-label' : undefined} 
+              _callback={() => setCurrentTimer('short')} 
+            />
+          </li>
+          <li>
+            <Button 
+              title="Long Break" 
+              activeClass={executing.active === 'long' ? 'active-label' : undefined} 
+              _callback={() => setCurrentTimer('long')} 
+            />
+          </li>
+        </ul>
+        <Button title="Settings" _callback={SettingsBtn} />
+        <div className="timer-container">
+          <div className="time-wrapper">
+              <CountdownAnimation
+                key={pomodoro} 
+                timer={pomodoro} 
+                animate={startAnimate}
+              >
+                {children}
+              </CountdownAnimation>
+          </div>
+        </div>
+        <div className="button-wrapper">
+          <Button title="Start" activeClass={!startAnimate ? 'active' : undefined} _callback={startTimer} />
+          <Button title="Pause" activeClass={startAnimate ? 'active' : undefined} _callback={pauseTimer} />
+        </div>
+      </> : <SetPomodoro />}
+    </div>
+  )
 }
 
-export default App;
+export default App
--- src/components/Timer/Timer.jsx
+++ src/components/Timer/Timer.jsx
@@ -1,13 +1,21 @@
 import "./Timer.css";
 
-const Timer = () => {
+const Timer = ({ time, isRunning, startTimer, stopTimer }) => {
 	return (
 		<section className="timer">
 			<div className="timer-container">
 				<div className="inner-circle">
-					<div className="timer-display">
-						<div className="time">17:59</div>
-						<span className="start-stop">pause</span>
+					<div
+						className="timer-display"
+						onClick={isRunning ? stopTimer : startTimer}
+					>
+						<div className="time">
+							{Math.floor(time / 60)}:
+							{(time % 60).toString().padStart(2, "0")}
+						</div>
+						<span className="start-stop">
+							{isRunning ? "pause" : "start"}
+						</span>
 					</div>
 				</div>
 			</div>
